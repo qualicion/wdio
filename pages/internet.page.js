341402a12ee738e5bbb93891467a89336d610987
toonday <qualicion@outlook.com>
2020-01-12 09:48:24 +0000

    Added hover elements and functions

--- pages/internet.page.js
+++ pages/internet.page.js
@@ -35,6 +35,24 @@ class Internet {
         return $('#password');
     }
 
+    figures(index){
+        return $(`.example .figure:nth-child(${index}) img`);
+    }
+
+    figureDetails(index){
+        return $(`.example .figure:nth-child(${index}) .figcaption h5`);
+    }
+
+    hoverOnFigure(index){
+        this.figures(index).waitForDisplayed();
+        this.figures(index).moveTo(1,1);
+    }
+
+    getFigureDetailsText(index){
+        this.figureDetails(index).waitForDisplayed();
+        return this.figureDetails(index).getText();
+    }
+
     enterUsername(text){
         this.username.waitForDisplayed();
         this.username.setValue(text);
