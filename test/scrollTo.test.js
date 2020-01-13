241e4927f2c86f9031678dcdfbdd16248d80d69e
toonday <qualicion@outlook.com>
2020-01-13 22:25:27 +0000

    Added scroll to footer test

---
+++ test/scrollTo.test.js
@@ -0,0 +1,15 @@
+internetPage = require('../pages/internet.page');
+
+describe('Scroll to element',() => {
+    it('should scroll to the footer', () => {
+        browser.url('/');
+        internetPage.pageHeader.waitForDisplayed();
+        internetPage.scrollToPageFooter();
+        assert.equal(true, internetPage.pageFooter.isDisplayedInViewport());
+    })
+
+    it('should scroll into view', () => {
+        internetPage.pageFooter.scrollIntoView();
+        assert.equal(true, internetPage.pageFooter.isDisplayedInViewport());
+    })
+})
