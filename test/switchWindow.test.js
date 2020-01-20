c1e07de7865e8db241a224e044f4021087b5a6ce
toonday <qualicion@outlook.com>
2020-01-20 23:34:35 +0000

    Added switch window test

---
+++ test/switchWindow.test.js
@@ -0,0 +1,13 @@
+internetPage = require('../pages/internet.page');
+
+
+describe('Switch window', () => {
+    it('should switch to the next window', () => {
+        browser.url(`${browser.options.baseUrl}/windows`);
+        internetPage.clickHereLink();
+        browser.switchWindow(`${browser.options.baseUrl}/windows/new`);
+        assert.equal(true, internetPage.h3Header.isExisting());
+        assert.equal(true, internetPage.h3Header.isDisplayed());
+        assert.equal('New Window', internetPage.h3Header.getText());
+    })
+})
