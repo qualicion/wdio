642925d5e6432a866974059306a10e1e0e39ba39
toonday <qualicion@outlook.com>
2020-01-21 10:38:21 +0000

    Added switch toiframe test

---
+++ test/switchToFrame.test.js
@@ -0,0 +1,12 @@
+internetPage = require('../pages/internet.page');
+
+describe('Switch to iframe', () => {
+    it('should switch to iframe',() =>{
+        browser.url(`${browser.options.baseUrl}/iframe`);
+        internetPage.h3Header.waitForDisplayed();
+        internetPage.iFrame.waitForDisplayed();
+        browser.switchToFrame(internetPage.iFrame);
+        internetPage.sendTextToBody('This is the text in the iFrame');
+        assert.equal('This is the text in the iFrame', internetPage.iFrameBody.getText());
+    })
+})
