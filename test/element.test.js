24053e56d79c95a75e529a287db84f45dd7a290a
toonday <qualicion@outlook.com>
2020-01-24 03:44:20 +0000

    Removed const for page object import

--- test/element.test.js
+++ test/element.test.js
@@ -1,4 +1,4 @@
-const internetPage = require('../pages/internet.page');
+internetPage = require('../pages/internet.page');
 
 describe('Interacting with elements', () => {
     it('Get text for element', () => {
