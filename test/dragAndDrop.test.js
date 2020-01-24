7780d82336adc45c536049bd66635d1887f53d24
toonday <qualicion@outlook.com>
2020-01-24 03:43:23 +0000

    Added test for drag and drop

---
+++ test/dragAndDrop.test.js
@@ -0,0 +1,16 @@
+internetPage = require('../pages/internet.page');
+
+describe('Drag and Drop', () => {
+    it.skip('should drag column A to column B', () => {
+        browser.url(`${browser.options.baseUrl}/drag_and_drop`);
+        internetPage.dragColumnAToColumnB();
+        browser.pause(10000);
+        assert.equal('A', internetPage.columnBHeader.getText());
+    })
+     it('should drag and drop', () => {
+        browser.url('https://crossbrowsertesting.github.io/drag-and-drop.html');
+        internetPage.draggableToDroppable();
+        browser.pause(5000);
+        assert.equal('Dropped!', internetPage.droppableParagraph.getText());
+    })    
+}) 
