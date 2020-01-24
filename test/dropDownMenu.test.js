50e27d48d6f19413183ac856e81cdeb62f6a0c12
toonday <qualicion@outlook.com>
2020-01-24 03:43:49 +0000

    Added test for dropdown

---
+++ test/dropDownMenu.test.js
@@ -0,0 +1,17 @@
+internetPage = require('../pages/internet.page');
+
+describe('Dropdown Menu', () => {
+    it('should select option 1', () => {
+        browser.url(`${browser.options.baseUrl}/dropdown`);
+        internetPage.clickDropDownMenu();
+        internetPage.clickDropDownMenuOption1();
+        assert.equal(true, internetPage.dropDownMenuOption1.isSelected());
+    })
+
+    it('should select option 2', () => {
+        internetPage.clickDropDownMenu();
+        internetPage.clickDropDownMenuOption2();
+        browser.pause(3000);
+        assert.equal(true, internetPage.dropDownMenuOption2.isSelected());
+    })
+})
