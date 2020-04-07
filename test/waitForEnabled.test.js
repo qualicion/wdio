7ab3cb14c4219988bed3789429bb685aff34564e
toonday <qualicion@outlook.com>
2020-04-07 14:01:32 +0100

    WDIO-3 increased pause

--- test/waitForEnabled.test.js
+++ test/waitForEnabled.test.js
@@ -4,13 +4,13 @@ describe('Wait for Enabled', () => {
     it('should wait for the input field to be enabled', () => {
         browser.url(`${browser.options.baseUrl}/dynamic_controls`);
         internetPage.clickEnableButton();
-        internetPage.inputEnabledField.waitForEnabled(4000);
+        internetPage.inputEnabledField.waitForEnabled(5000);
         assert.equal(true, internetPage.inputEnabledField.isEnabled());
     })
 
     it('should wait for the input field to be disabled', () => {
         internetPage.clickEnableButton();
-        internetPage.inputEnabledField.waitForEnabled(4000, true);
+        internetPage.inputEnabledField.waitForEnabled(5000, true);
         assert.equal(true, internetPage.inputEnabledField.isEnabled());
     })
 });
