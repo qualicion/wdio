4e986ba97854fd162977a7f95064ed95cfe1f036
toonday <qualicion@outlook.com>
2020-04-13 03:24:13 +0100

    Added wait until coverage

---
+++ wdio/test/waitUntil.test.js
@@ -0,0 +1,21 @@
+const internetPage = require('../../wdio/pages/internet.page');
+
+
+describe('Wait Until', () => {
+    it('should wait until the button text changes to add', () => {
+        browser.url(`${browser.options.baseUrl}/dynamic_controls`);
+        internetPage.clickPageBtn();
+        browser.waitUntil(() => {
+            return internetPage.pageButton.getText() === 'Add'
+        }, 6000 , 'Expect button text to change')
+        assert.equal('Add', internetPage.pageButton.getText());
+    })
+
+    it('should wait until the button text changes to remove', () => {
+        internetPage.clickPageBtn();
+        browser.waitUntil(() => {
+            return internetPage.pageButton.getText() === 'Remove'
+        }, 6000 , 'Expect button text to change')
+        assert.equal('Remove', internetPage.pageButton.getText());
+    })
+})
