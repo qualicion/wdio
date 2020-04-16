616963cfa0c1e0497f780fb96b8039d668109866
toonday <qualicion@outlook.com>
2020-04-16 20:08:34 +0100

    WDIO-11 Added test coverage for valid logged in user

--- wdio/test/exam.test.js
+++ wdio/test/exam.test.js
@@ -1,1 +1,31 @@
-const internetPage = require('../../wdio/pages/internet.page');
+const loginPage = require('../../wdio/pages/login.page');
+const loginData = require('../../data/login-data');
+const secureAreaPage = require('../../wdio/pages/secure-area.page');
+
+
+describe('Verify that user can login and logout successfully', () => {
+    it('Navigate to the login page', () => {
+        browser.url(`${browser.options.baseUrl}/login`);
+    })
+
+    it('Verify user is on the login page', () => {
+        loginPage.getLoginPageHeader();
+    })
+
+    it('Enter valid username and password', () => {
+        loginPage.enterUserName(loginData.valid.username);
+        loginPage.enterPassword(loginData.valid.password);
+    })
+
+    it('User is logged in successfully', () => {
+        loginPage.clickLoginBtn();
+        secureAreaPage.getLoginSuccessMessage();
+        secureAreaPage.getLogoutBtn();
+    })
+
+    it('User is able to logout successfully', () => {
+        secureAreaPage.clickLogOutBtn();
+        loginPage.getLogoutSuccessMessage();
+        browser.pause();
+    })
+})
