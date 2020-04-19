7c1a5ee7dab20200785547bd64e3ee5073a90a00
toonday <qualicion@outlook.com>
2020-04-19 10:36:27 +0100

    WDIO-11 fixed test and removed  skip from it block

--- wdio/test/dragAndDrop.test.js
+++ wdio/test/dragAndDrop.test.js
@@ -1,12 +1,12 @@
 const internetPage = require('../../wdio/pages/internet.page');
 
 describe('Drag and Drop', () => {
-    it.skip('should drag column A to column B', () => {
+    it('should drag column A to column B', () => {
         browser.url(`${browser.options.baseUrl}/drag_and_drop`);
         internetPage.dragColumnAToColumnB();
-        browser.pause(10000);
-        assert.equal('A', internetPage.columnBHeader.getText());
+        assert.equal('B', internetPage.columnBHeader.getText());
     })
+    
      it('should drag and drop', () => {
         browser.url('https://crossbrowsertesting.github.io/drag-and-drop.html');
         internetPage.draggableToDroppable();
